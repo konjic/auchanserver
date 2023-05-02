98aecccccaa794cb7e063d1f771e974ec018e76e
johnnythehobo <razvanburloiu@gmail.com>
2023-05-02 20:59:26 +0300

    t

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const dotenv = require('dotenv').config()
 const express = require('express');
 
-const userRouter = require('../src/routes/users')
+const userRouter = require('./src/routes/users')
 
-const connectDB = require('../config/connectDb.js')
+const connectDB = require('./config/connectDb.js')
 
 const app = express()
 const cors =require("cors") 
