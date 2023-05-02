538c0e485df981393f91fd6651a47eaa1af9e61a
johnnythehobo <razvanburloiu@gmail.com>
2023-05-02 21:26:52 +0300

    model

--- src/models/Users.js
+++ src/models/Users.js
@@ -3,6 +3,8 @@ const mongoose =require("mongoose")
 const UserSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    name: {type: String, required: true},
+    email: {type: String, required: true},
 })
 
 // export const UserModel = mongoose.model("users",UserSchema)
